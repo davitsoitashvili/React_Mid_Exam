83c0b662078998445bc1c3f0f6a3c9a6a61491b5
DavitSoitashvili <davit.soitashvili.1@btu.edu.ge>
2020-11-05 21:14:01 +0400

    add dispay posts and comments in alert

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,34 +1,50 @@
-import React, { useState } from "react";
-import { ButtonType, COMMENTS_url, LogOutButton, POSTS_URL, removeToken } from "../../constants";
+import React from "react";
+import {
+  ButtonType,
+  COMMENTS_url,
+  LogOutButton,
+  POSTS_URL,
+} from "../../constants";
 import GetPosts from "../../service/postsService";
 import Button from "../presentation/Button";
 
-
-const onClickPostsButton = () => {
-    var posts = GetPosts(POSTS_URL)
+function Menu({ onClick }) {
+  const onClickPostsButton = () => {
+    GetPosts(POSTS_URL)
       .then((respone) => respone.json())
-      .then((respone) => console.log(respone));
+      .then((respone) => {
+        var posts = [];
+        respone.forEach((post) => {
+          posts.push(
+            `user ID : ${post.id} id : ${post.id} title : ${post.title} body : ${post.body}`
+          );
+        });
+        alert(posts);
+      });
   };
-  
+
   const onClickCommentsButton = () => {
-    var comments = GetPosts(COMMENTS_url)
+    GetPosts(COMMENTS_url)
       .then((respone) => respone.json())
-      .then((respone) => console.log(respone));
+      .then((respone) => {
+        var comments = [];
+        respone.forEach((post) => {
+            comments.push(
+            `user ID : ${post.id} id : ${post.id} name : ${post.name} email : ${post.email} body: ${post.body}`
+          );
+        });
+        alert(comments);
+      });
   };
 
-function Menu({onClick}) {
-    const [hasPosts, posts] = useState(false);
-    const [hasComments, getComments] = useState(false);
-
- 
-    return (
-        <>
-        <Button {...ButtonType.posts} onClick={onClickPostsButton}/>
-        <Button {...ButtonType.comments} onClick={onClickCommentsButton}/>
-        <hr/>
-        <Button {...LogOutButton} onClick={onClick}/>
-        </>
-    )
+  return (
+    <>
+      <Button {...ButtonType.posts} onClick={onClickPostsButton} />
+      <Button {...ButtonType.comments} onClick={onClickCommentsButton} />
+      <hr />
+      <Button {...LogOutButton} onClick={onClick} />
+    </>
+  );
 }
 
 export default Menu;
